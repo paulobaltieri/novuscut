201ab64f10d0778ef715d53e33cb05ee8594388a
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2025-04-20 23:09:00 -0300

    Adicionado botões e input ao componente Loginpage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import anthemImg from "../../images/anthem.jpg";
+import Avatar from "@mui/material/Avatar";
+import avatarPerson from "../../images/man01.png";
 import "../LoginPage/LoginPage.css";
 import { TextField } from "@mui/material";
-
+import Button from "@mui/material/Button";
 export default function LoginPage() {
   return (
     <Card className="login-card">
-      <img src={anthemImg} className="login-avatar" alt="Imagem Login" />
-      <div className="login-container">
-        <TextField className="login-user" 
-        label="Usuário"
-        size="small" />
-      </div>
-    </Card>
+      <Avatar
+        className="login-avatar"
+        alt="Remy Sharp"
+        src={avatarPerson}
+        sx={{ width: "190px", height: "190px" }}
+      />
+        <div className="login-container">
+          <TextField
+            className="login-user"
+            label="Usuário"
+            value=""
+            size="small"
+          />
+          <TextField className="login-password" label="Senha" size="small" />
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#19315e", width: "300px" }}
+          >
+            Entrar
+          </Button>
+        </div>
+      </Card>
   );
 }
