f8e392d28b62e8f5d379cd0b208869622e3a1d3b
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2025-04-23 22:02:18 -0300

    Estilização cartões da pagina home

--- src/pages/Home/homePage.jsx
+++ src/pages/Home/homePage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Appbar from "../../components/Appbar/appbar";
 import "../Home/homePage.css";
+import { Card } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="papelParede" 
-    style={{backgroundColor:"red",
-        height:"100%"
-    }}>
+    <div>
       <Appbar />
-      <span>Olá mundo</span>
+      <div className="card-container">
+        <Card className="custom-card" style={{backgroundColor:"#27c281"}}>Card 1</Card>
+        <Card className="custom-card" style={{backgroundColor:"#e7505a"}}>Card 2</Card>
+        <Card className="custom-card" style={{backgroundColor:"#6f75bf"}}>Card 3</Card>
+        <Card className="custom-card" style={{backgroundColor:"#393939"}}>Card 4</Card>
+      </div>
     </div>
   );
 }
