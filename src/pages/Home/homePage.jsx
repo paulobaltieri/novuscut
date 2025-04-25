e822e6480278aae790df8f58f98b88cea91f18eb
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2025-04-24 23:34:47 -0300

    Correção dos cards

--- src/pages/Home/homePage.jsx
+++ src/pages/Home/homePage.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="card-container">
         <Card className="custom-card" style={{backgroundColor:"#27c281"}}>Card 1</Card>
         <Card className="custom-card" style={{backgroundColor:"#e7505a"}}>Card 2</Card>
-        <Card className="custom-card" style={{backgroundColor:"#6f75bf"}}>Card 3</Card>
+        <Card className="custom-card" style={{backgroundColor:"#0393ad"}}>Card 3</Card>
         <Card className="custom-card" style={{backgroundColor:"#393939"}}>Card 4</Card>
       </div>
     </div>
