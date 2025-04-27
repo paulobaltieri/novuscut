bc84c8daf9d3e0c18c2c8e719a62f7c077797f0e
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2025-04-27 08:41:05 -0300

    Adicionado a opçao de confirmaçao a tela de saida do sistema

--- src/components/Dialog/alertDialog.jsx
+++ src/components/Dialog/alertDialog.jsx
@@ -1,12 +1,17 @@
-import React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import { useNavigate } from "react-router-dom";
 
 export default function AlertDialog({ open, onClose }) {
+  const navigate = useNavigate();
+  function ReturnLoginPage() {
+    navigate("/");
+  }
+
   return (
     <div>
       <Dialog open={open} onClose={onClose}>
@@ -19,15 +24,15 @@ export default function AlertDialog({ open, onClose }) {
             onClick={onClose}
             autoFocus
             variant="contained"
-            sx={{ width: "120px" , backgroundColor:"#ff8787"}}
+            sx={{ width: "120px", backgroundColor: "#ff8787" }}
           >
             Cancelar
           </Button>
           <Button
-            onClick={onClose}
+            onClick={ReturnLoginPage}
             autoFocus
             variant="contained"
-            sx={{ width: "120px" , backgroundColor:"#0dcb91"}}
+            sx={{ width: "120px", backgroundColor: "#0dcb91" }}
           >
             Confirmar
           </Button>
