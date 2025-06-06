937a2b14f075b98682ad1baf5665c96e2d511753
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2025-06-05 22:02:46 -0300

    Adiçao de tabelas a pagina barbeuris

--- src/pages/Clients/ClientsPage.jsx
+++ src/pages/Clients/ClientsPage.jsx
@@ -1,18 +1,78 @@
 import React from "react";
 import Navbar from "../../components/Navbar/navbar";
 import { Card } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import "../../pages/Clients/ClientsPage.css";
+
+function createData(id, name, email, telefone, foto, status) {
+  return { id, name, email, telefone, foto, status };
+}
+
+const rows = [
+  createData(
+    "01",
+    "Paulo Baltieri",
+    "baltieri@baltieri.com",
+    "(71)9899-9878u",
+    "teste",
+    "Disponivel"
+  ),
+  createData(
+    "02",
+    "Diego Baltieri",
+    "baltieri@baltieri.com",
+    "(71)9899-9878u",
+    "teste",
+    "Disponivel"
+  ),
+  createData(
+    "03",
+    "Debora",
+    "baltieri@baltieri.com",
+    "(71)9899-9878u",
+    "teste",
+    "Disponivel"
+  ),
+];
 
 export default function ClientsListTable() {
   return (
-    <>
-      <Navbar />
-      <Card sx={{width:"96%",margin:"20px auto", padding:"10px"}}>
-        <label className="titleClients">Clientes</label>
-      </Card>
-      <Card sx={{width:"96%",margin:"20px auto", padding:"10px"}}>
-        Teste
-      </Card>
-    </>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="right">Nome</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Telefone</TableCell>
+            <TableCell align="right">Foto</TableCell>
+            <TableCell align="right">Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.telefone}</TableCell>
+              <TableCell align="right">{row.foto}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
