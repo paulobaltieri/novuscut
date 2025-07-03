e27283347a10fb69fd17483973aee174704ebed2
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2025-07-02 21:47:43 -0300

    Avatar baseado no sexo do usuario.

--- src/pages/Barber/BarberPage.jsx
+++ src/pages/Barber/BarberPage.jsx
@@ -5,15 +5,13 @@ import BarberMale from "../../images/man01.png"
 import BarberMale02 from "../../images/man02.png"
 import BarberFemale from "../../images/woamn02.png"
 
-
-const avatarList = [BarberMale, BarberMale02, BarberFemale]
 const CreateCardBarber = (foto, nome, age, sexo, disponibilidade) => {
   return { foto, nome, age, sexo, disponibilidade }
 }
 
 const barberCad = [
   CreateCardBarber(
-    { BarberMale },
+    "Foto",
     "Isaac",
     25,
     "Masculino",
@@ -21,14 +19,14 @@ const barberCad = [
   ),
 
   CreateCardBarber(
-    { BarberMale02},
+    "Foto",
     "Paulo",
     36,
     "Masculino",
     "Tarde"
   ),
   CreateCardBarber(
-     { BarberFemale },
+    "Foto",
     "Jeruza Zuleiko",
     24,
     "Feminino",
@@ -37,7 +35,15 @@ const barberCad = [
 
 ]
 
+let sexoPerson = barberCad.map((barber, index) => {
+  if (barber.sexo === "Feminino") return BarberFemale
+  return index % 2 === 0 ? BarberMale : BarberMale02
+})
+
+console.log(sexoPerson)
+
 export default function BarberList() {
+
   return (
     <>
       <Navbar />
@@ -52,9 +58,10 @@ export default function BarberList() {
           return (
             <Card key={index} sx={{ backgroundColor: "red", paddingTop: "30px", width: "230px", height: "290px" }}>
               <Avatar
-                src={avatarList[index]}
+                src={sexoPerson[index]}
                 style={{ width: "150px", height: "150px", margin: "auto" }}
               />
+
               <CardContent sx={{ textAlign: "center" }}>
                 <Typography key={index}>
                   Nome: {barber.nome} <br />
